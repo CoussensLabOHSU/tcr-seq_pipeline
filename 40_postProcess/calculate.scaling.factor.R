bc77042588b116a0b3fa37779465ac4f0b0bdb67
Ana <howellsf@ohsu.edu>
2021-04-27 16:52:05 -0700

    40_postProcess/calculate.scaling.factor.R

--- 40_postProcess/calculate.scaling.factor.R
+++ 40_postProcess/calculate.scaling.factor.R
@@ -10,7 +10,7 @@ library(data.table)
 
 ### Get command line arguments
 arguments <- commandArgs(trailingOnly=TRUE);
-spike.count.dir <- arguments[1];	# DNAXXXXLC/normalization/counts/
+spike.count.dir <- arguments[1];	# LIBXXXXXXLC/normalization/counts/LIBXXXXXXLC_SXX.assembled.spike.counts.25bp.txt
 ref.file <- arguments[2];		# /tcr_sequencing_tools/reference/text_barcodesvj.txt
     
 ###   Get list of files in directory
@@ -45,16 +45,59 @@ for(i in 1:length(spike.count.files))   {
     counts.and.samples[,i] <- curr.spike.counts$spike.count;
 }   #   for
 
+### method Zero-to-One deprecated after testing multiple methods for inputation
+### Current method uses a global mean
+
+## Remove spikes which sum is zero (adding these steps generated and Error)
+# TODO
+#empty_v <- which(apply(counts.and.samples, 1, sum) == 0) #remove spikes which sum is equal to zero
+#keep_v <- setdiff(rownames(counts.and.samples), names(empty_v))
+
+#counts.and.samples <- counts.and.samples[rownames(counts.and.samples) %in% keep_v,]
+
+# zero-to-one method (deprecated)
+
 ## Add one to all cells to make sure to avoid having zeroes
-counts.and.samples <- apply(counts.and.samples, c(1,2), function(x) x + 1)
+#counts.and.samples <- apply(counts.and.samples, c(1,2), function(x) x + 1)
 
 ###   Calculate mean number of spikes found, across all samples
-sum.across.samples <- apply(counts.and.samples, 1, sum);
-spike.count.mean <- sum(sum.across.samples) / num.rows;
+#sum.across.samples <- apply(counts.and.samples, 1, sum);
+#spike.count.mean <- sum(sum.across.samples) / num.rows;
 
 ###   Use the mean to calculate the scaling factor
-scaling.factor <- sum.across.samples / spike.count.mean;
-scaling.factor <- 1 / scaling.factor;
+#scaling.factor <- sum.across.samples / spike.count.mean;
+#scaling.factor <- 1 / scaling.factor;
+
+
+#######################
+### Global Method ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#######################
+
+### turn Zero to NA
+zeroToNA_df <- apply(counts.and.samples, c(1,2), function(x) { if (x == 0) {
+    return(NA) } else { return(x)}
+})
+
+### Calculate the total amount of each spike found in all samples (per-spike total)
+ztna_sum.across.samples <- apply(zeroToNA_df, 1, function(x) sum(x, na.rm = T))
+
+### Find the average per-spike count among all 260
+ztna_spike.count.mean <- sum(ztna_sum.across.samples) / nrow(counts.and.samples)
+
+### Turn Zero to Mean (using the mean value found from the NA to zero)
+zeroToMean_df <- apply(counts.and.samples, c(1,2), function(x) { if (x == 0) {
+    return(ztna_spike.count.mean)} else { return(x)}})
+
+### Calculate the total amount of each spike found in all samples (per-spike total)
+ztm_sum.across.samples <- apply(zeroToMean_df, 1, function(x) sum(x, na.rm = T))
+
+### Find the average per-spike count among all 260
+ztm_spike.count.mean <- sum(ztm_sum.across.samples) / nrow(counts.and.samples)
+
+### Calculate scaling factor
+### Divide each individual spike sum by the mean overall and invert
+ztm_scaling.factor <- 1 / (ztm_sum.across.samples / ztm_spike.count.mean)
+
 
 ####### Have to now add entry for V122 because we use the same spike/scaling factor for V121 and V122 #######
 
@@ -62,7 +105,7 @@ scaling.factor <- 1 / scaling.factor;
 ref_spikes <- fread(ref.file)
 
 ###	Combine V and J columns with spike counts
-temp.vj <- as.data.frame(cbind(ref_spikes$V, ref_spikes$J, scaling.factor), stringsAsFactors = F)
+temp.vj <- as.data.frame(cbind(ref_spikes$V, ref_spikes$J, ztm_scaling.factor), stringsAsFactors = F)
 ###   Extract the V121/V122 spike
 v.122 <- temp.vj[temp.vj$V1 == "V12-1-2-",]
 ###   rename V12-1-2 to V121 and V122
